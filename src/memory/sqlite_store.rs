98f7a77fac5b520d42e417d45255faa7461d0d9f
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-03-07 10:48:18 +0100

    feat:async

--- src/memory/sqlite_store.rs
+++ src/memory/sqlite_store.rs
@@ -1,13 +1,20 @@
 use crate::{Address, MemoryADT};
-use r2d2::Pool;
-use r2d2_sqlite::SqliteConnectionManager;
+use async_sqlite::{JournalMode, Pool, PoolBuilder};
 use rusqlite::{OptionalExtension, params_from_iter, types::Value::Blob};
-use std::{collections::HashMap, fmt, marker::PhantomData, ops::Deref, path::Path};
+use std::{
+    collections::HashMap,
+    fmt::{self, Debug},
+    marker::PhantomData,
+    ops::Deref,
+    path::Path,
+    sync::Arc,
+};
 
 #[derive(Debug)]
 pub enum SqliteMemoryError {
     SqlError(rusqlite::Error),
     PoolingError(r2d2::Error),
+    AsyncSqliteError(async_sqlite::Error),
 }
 
 impl std::error::Error for SqliteMemoryError {}
@@ -17,6 +24,7 @@ impl fmt::Display for SqliteMemoryError {
         match self {
             Self::SqlError(err) => write!(f, "Sql store memory error: {}", err),
             Self::PoolingError(err) => write!(f, "r2d2 pooling error: {}", err),
+            Self::AsyncSqliteError(err) => write!(f, "async-sqlite error: {}", err),
         }
     }
 }
@@ -33,22 +41,37 @@ impl From<r2d2::Error> for SqliteMemoryError {
     }
 }
 
-#[derive(Debug, Clone)]
+impl From<async_sqlite::Error> for SqliteMemoryError {
+    fn from(err: async_sqlite::Error) -> Self {
+        Self::AsyncSqliteError(err)
+    }
+}
+
+// TODO : mix pragma and wal calls
+#[derive(Clone)]
 pub struct SqliteMemory<Address, Word> {
-    pool: Pool<SqliteConnectionManager>,
+    pool: Pool,
     _marker: PhantomData<(Address, Word)>,
 }
 
-const CREATE_TABLE_SCRIPT: &str = "
-CREATE TABLE IF NOT EXISTS memory
-(
+impl<Address, Word> Debug for SqliteMemory<Address, Word> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("SqliteMemory")
+            .field("pool", &"<async_sqlite::Pool>")
+            .field("_marker", &PhantomData::<(Address, Word)>)
+            .finish()
+    }
+}
+
+const CREATE_TABLE_SCRIPT: &str = "PRAGMA synchronous = NORMAL;
+CREATE TABLE IF NOT EXISTS memory (
     a BLOB PRIMARY KEY,
     w BLOB NOT NULL
-)";
+);";
 
 impl<Address, Word> SqliteMemory<Address, Word> {
     /// Connects to a known DB using the given path.
-    pub fn connect(path: &impl AsRef<Path>) -> Result<Self, SqliteMemoryError> {
+    pub async fn connect(path: &str) -> Result<Self, SqliteMemoryError> {
         // SqliteConnectionManager::file is the equivalent of using
         // `rusqlite::Connection::open` as documented in the function's source
         // code. Two pragmas are set to drastically improve performance :
@@ -56,13 +79,15 @@ impl<Address, Word> SqliteMemory<Address, Word> {
         //   safe from corruption with synchronous=NORMAL
         // - synchronous = NORMAL : fsync only in critical moments, drastically improving performance as
         //   fsync is a very expensive operation.
-        let manager = SqliteConnectionManager::file(path.as_ref()).with_init(|conn| {
-            conn.pragma_update(None, "journal_mode", "WAL")?;
-            conn.pragma_update(None, "synchronous", "NORMAL")?;
-            Ok(())
-        });
-        let pool = r2d2::Pool::builder().build(manager)?;
-        pool.get()?.execute(CREATE_TABLE_SCRIPT, [])?;
+        let pool = PoolBuilder::new()
+            .path(path)
+            .journal_mode(JournalMode::Wal)
+            .open()
+            .await
+            .unwrap();
+
+        pool.conn(|conn| conn.execute_batch(CREATE_TABLE_SCRIPT))
+            .await?;
 
         Ok(Self {
             pool,
@@ -82,27 +107,32 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize> MemoryADT
         &self,
         addresses: Vec<Self::Address>,
     ) -> Result<Vec<Option<Self::Word>>, Self::Error> {
-        let conn = self.pool.get()?;
+        let adr_arc = Arc::new(addresses);
+        let adr_arc_clone = Arc::clone(&adr_arc);
+
+        let mut bindings = self
+            .pool
+            .conn(move |conn| {
+                conn.prepare(&format!(
+                    "SELECT a, w FROM memory WHERE a IN ({})",
+                    vec!["?"; adr_arc_clone.len()].join(",")
+                ))?
+                .query_map(
+                    params_from_iter(adr_arc_clone.iter().map(Deref::deref)),
+                    |row| {
+                        let a = Address::from(row.get::<_, [u8; ADDRESS_LENGTH]>(0)?);
+                        let w = row.get(1)?;
+                        Ok((a, w))
+                    },
+                )?
+                .collect::<Result<HashMap<_, _>, _>>()
+            })
+            .await?;
 
         // Return order of an SQL select statement is undefined, and
         // mismatched are ignored. A post-processing is thus needed to
         // generate a returned value complying to the batch-read spec.
-        let mut bindings = conn
-            .prepare(&format!(
-                "SELECT a, w FROM memory WHERE a IN ({})",
-                vec!["?"; addresses.len()].join(",")
-            ))?
-            .query_map(
-                params_from_iter(addresses.iter().map(Deref::deref)),
-                |row| {
-                    let a = Address::from(row.get::<_, [u8; ADDRESS_LENGTH]>(0)?);
-                    let w = row.get(1)?;
-                    Ok((a, w))
-                },
-            )?
-            .collect::<Result<HashMap<_, _>, _>>()?;
-
-        Ok(addresses.iter().map(|addr| bindings.remove(addr)).collect())
+        Ok(adr_arc.iter().map(|addr| bindings.remove(addr)).collect())
     }
 
     async fn guarded_write(
@@ -112,28 +142,35 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize> MemoryADT
     ) -> Result<Option<Self::Word>, Self::Error> {
         let (ag, wg) = guard;
 
-        let mut conn = self.pool.get()?;
-        let tx = conn.transaction_with_behavior(rusqlite::TransactionBehavior::Immediate)?;
+        let current_word = self
+            .pool
+            .conn_mut(move |conn| {
+                let tx =
+                    conn.transaction_with_behavior(rusqlite::TransactionBehavior::Immediate)?;
 
-        let current_word = tx
-            .query_row("SELECT w FROM memory WHERE a = ?", [&*ag], |row| row.get(0))
-            .optional()?;
+                let current_word = tx
+                    .query_row("SELECT w FROM memory WHERE a = ?", [&*ag], |row| row.get(0))
+                    .optional()?;
 
-        if current_word == wg {
-            tx.execute(
-                &format!(
-                    "INSERT OR REPLACE INTO memory (a, w) VALUES {}",
-                    vec!["(?,?)"; bindings.len()].join(",")
-                ),
-                params_from_iter(
-                    // There seems to be no way to avoid cloning here.
-                    bindings
-                        .iter()
-                        .flat_map(|(a, w)| [Blob(a.to_vec()), Blob(w.to_vec())]),
-                ),
-            )?;
-            tx.commit()?;
-        }
+                if current_word == wg {
+                    tx.execute(
+                        &format!(
+                            "INSERT OR REPLACE INTO memory (a, w) VALUES {}",
+                            vec!["(?,?)"; bindings.len()].join(",")
+                        ),
+                        params_from_iter(
+                            bindings
+                                .iter()
+                                // There seems to be no way to avoid cloning here.
+                                .flat_map(|(a, w)| [Blob(a.to_vec()), Blob(w.to_vec())]),
+                        ),
+                    )?;
+                    tx.commit()?;
+                }
+
+                Ok(current_word)
+            })
+            .await?;
 
         Ok(current_word)
     }
@@ -154,28 +191,25 @@ mod tests {
 
     #[tokio::test]
     async fn test_rw_seq() -> Result<(), SqliteMemoryError> {
-        let m = SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(&Path::new(
-            DB_PATH,
-        ))?;
+        let m =
+            SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_PATH).await?;
         test_single_write_and_read(&m, rand::random()).await;
         Ok(())
     }
 
     #[tokio::test]
     async fn test_guard_seq() -> Result<(), SqliteMemoryError> {
-        let m = SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(&Path::new(
-            DB_PATH,
-        ))?;
+        let m =
+            SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_PATH).await?;
         test_wrong_guard(&m, rand::random()).await;
         Ok(())
     }
 
     #[tokio::test]
     async fn test_rw_ccr() -> Result<(), SqliteMemoryError> {
-        let m = SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(&Path::new(
-            DB_PATH,
-        ))?;
-        test_guarded_write_concurrent(&m, rand::random(), Some(1000)).await;
+        let m =
+            SqliteMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_PATH).await?;
+        test_guarded_write_concurrent(&m, rand::random(), Some(100)).await;
         Ok(())
     }
 }
