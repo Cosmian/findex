96a68135c20484634138f504b007f7f527c35905
HatemMn <19950216+HatemMn@users.noreply.github.com>
2025-03-18 18:44:18 +0100

    feat: from async to sync

--- src/memory/postgresql_store.rs
+++ src/memory/postgresql_store.rs
@@ -1,13 +1,15 @@
 use crate::{Address, MemoryADT};
+use deadpool_postgres::{Config, CreatePoolError, Runtime};
 use std::fmt;
 use std::marker::PhantomData;
 use std::sync::Arc;
-use tokio_postgres::{Client, NoTls}; // Add to imports
+use tokio_postgres::{Client, Connection, NoTls, Socket, Statement, tls::MakeTlsConnect};
 
 #[derive(Debug)]
 pub enum PostgresMemoryError {
     AsyncPostgresError(tokio_postgres::Error),
     TryFromSliceError(std::array::TryFromSliceError),
+    CreatePoolError(deadpool_postgres::CreatePoolError),
     TableCreationError(u64),
     InvalidDataLength(usize),
 }
@@ -33,7 +35,14 @@ impl fmt::Display for PostgresMemoryError {
                     len
                 )
             }
+            Self::CreatePoolError(err) => write!(f, "deadpool_postgres error: {}", err),
         }
+    }
+}
+
+impl From<CreatePoolError> for PostgresMemoryError {
+    fn from(err: CreatePoolError) -> Self {
+        Self::CreatePoolError(err)
     }
 }
 
@@ -52,26 +61,53 @@ impl From<std::array::TryFromSliceError> for PostgresMemoryError {
 #[derive(Clone, Debug)]
 pub struct PostGresMemory<Address, Word> {
     client: Arc<Client>,
+    select_stmt: Arc<Statement>,
+    insert_stmt: Arc<Statement>,
     _marker: PhantomData<(Address, Word)>,
 }
-// Attack Surface Reduction:
-//     92% SQLi risk reduction via prepared statements
-//     Column-level encryption for sensitive payloads
 
-// The table field sizes are fixed at compile time, so we should bound the generic parameters upon connecting
-// to the DB
+const B_READ_STMT: &str = "
+    SELECT f.w
+    FROM UNNEST($1::bytea[]) WITH ORDINALITY AS params(addr, idx)
+    LEFT JOIN findex_db f ON params.addr = f.a
+    ORDER BY params.idx;
+";
+
+const G_WRITE_STMT: &str = "
+    WITH
+    guard_check AS (
+        SELECT w FROM findex_db WHERE a = $1::bytea
+    ),
+    dedup_input_table AS (
+    SELECT DISTINCT ON (a) a, w
+        FROM UNNEST($3::bytea[], $4::bytea[]) WITH ORDINALITY AS t(a, w, order_idx)
+        ORDER BY a, order_idx DESC),
+    insert_cte AS (
+    INSERT INTO findex_db (a, w)
+    SELECT a, w FROM dedup_input_table AS t(a,w)
+    WHERE (
+        $2::bytea IS NULL AND NOT EXISTS (SELECT 1 FROM guard_check)
+    ) OR (
+        $2::bytea IS NOT NULL AND EXISTS (
+            SELECT 1 FROM guard_check WHERE w = $2::bytea
+        )
+    )
+    ON CONFLICT (a) DO UPDATE SET w = EXCLUDED.w
+    )
+    SELECT COALESCE((SELECT w FROM guard_check)) AS original_guard_value;
+";
+
 impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize>
     PostGresMemory<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>
 {
-    // this assumes that :
-    // - the database is already created and called findex_db
-    // - the user cosmian_findex exists and has a password with md5 auth
-    // TODO(hatem) : make pathu usable
-    pub async fn connect(_path: &str) -> Result<Self, PostgresMemoryError> {
-        // TODO(hatem): idk if keep connecting as postgres def user ?
-        // https://users.rust-lang.org/t/not-able-to-connect-postgres-using-rust/76989
-        let (client, connection) = tokio_postgres::connect(_path, NoTls).await?;
-
+    pub async fn connect<T>(
+        client: Client,
+        connection: Connection<Socket, T::Stream>,
+    ) -> Result<Self, PostgresMemoryError>
+    where
+        T: MakeTlsConnect<Socket>,
+        T::Stream: Send + 'static, // 'static bound simply ensures that the type is fully owned
+    {
         // The connection object performs the actual communication with the database
         // `Connection` only resolves when the connection is closed, either because a fatal error has
         // occurred, or because its associated `Client` has dropped and all outstanding work has completed.
@@ -97,8 +133,70 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize>
         if returned != 0 {
             return Err(PostgresMemoryError::TableCreationError(returned));
         }
+
+        let _ = client
+            .execute(
+                "
+                ALTER TABLE findex_db
+                ALTER COLUMN a SET STORAGE EXTERNAL,
+                ALTER COLUMN w SET STORAGE EXTERNAL;",
+                &[],
+            )
+            .await?;
+        let select_stmt = client.prepare(B_READ_STMT).await?;
+        let insert_stmt = client.prepare(G_WRITE_STMT).await?;
+
         Ok(Self {
             client: Arc::new(client),
+            select_stmt: Arc::new(select_stmt),
+            insert_stmt: Arc::new(insert_stmt),
+            _marker: PhantomData,
+        })
+    }
+
+    pub async fn connect_with_pool<T>(url: &str) -> Result<Self, PostgresMemoryError>
+    where
+        T: MakeTlsConnect<Socket>,
+        T::Stream: Send + 'static, // 'static bound simply ensures that the type is fully owned
+    {
+        let mut cfg = Config::new();
+        cfg.url = Some("postgres://cosmian_findex:cosmian_findex@localhost/cosmian?application_name=findex_rust&sslmode=prefer".to_string());
+        let pool = cfg.create_pool(Some(Runtime::Tokio1), NoTls)?;
+        let client = pool.get().await.unwrap(); // TODO not in prod !!!
+
+        let returned = client
+            .execute(
+                &format!(
+                    "
+                    CREATE TABLE IF NOT EXISTS findex_db (
+                        a BYTEA PRIMARY KEY CHECK (octet_length(a) = {}),
+                        w BYTEA NOT NULL CHECK (octet_length(w) = {})
+                    );",
+                    ADDRESS_LENGTH, WORD_LENGTH
+                ),
+                &[],
+            )
+            .await?;
+        if returned != 0 {
+            return Err(PostgresMemoryError::TableCreationError(returned));
+        }
+
+        let _ = client
+            .execute(
+                "
+                ALTER TABLE findex_db
+                ALTER COLUMN a SET STORAGE EXTERNAL,
+                ALTER COLUMN w SET STORAGE EXTERNAL;",
+                &[],
+            )
+            .await?;
+        let select_stmt = client.prepare(B_READ_STMT).await?;
+        let insert_stmt = client.prepare(G_WRITE_STMT).await?;
+
+        Ok(Self {
+            client: Arc::new(client),
+            select_stmt: Arc::new(select_stmt),
+            insert_stmt: Arc::new(insert_stmt),
             _marker: PhantomData,
         })
     }
@@ -121,20 +219,12 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize> MemoryADT
         addresses: Vec<Self::Address>,
     ) -> Result<Vec<Option<Self::Word>>, Self::Error> {
         self.client
-            // the left join is necessary ensure that the order of the addresses is preserved
+            // the left join is necessary to ensure that the order of the addresses is preserved
             // as well as to return None for addresses that don't exist
-            .query(
-                "
-                    SELECT f.w 
-                    FROM unnest($1::bytea[]) WITH ORDINALITY AS params(addr, idx) 
-                    LEFT JOIN findex_db f ON params.addr = f.a 
-                    ORDER BY params.idx;
-                ",
-                &[&addresses
-                    .iter()
-                    .map(|addr| addr.as_slice())
-                    .collect::<Vec<_>>()],
-            )
+            .query(&*self.select_stmt, &[&addresses
+                .iter()
+                .map(|addr| addr.as_slice())
+                .collect::<Vec<_>>()])
             .await?
             .iter()
             .map(|row| {
@@ -166,39 +256,15 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize> MemoryADT
             bindings.into_iter().map(|(a, w)| (*a, w)).unzip();
 
         self.client
-            .query_opt(
-                "
-        WITH
-        guard_check AS (
-            SELECT w FROM findex_db WHERE a = $1::bytea
-        ),
-        temp_table AS (SELECT DISTINCT ON (a) a, w
-            FROM UNNEST($3::bytea[], $4::bytea[]) WITH ORDINALITY AS t(a, w, order_idx)
-            ORDER BY a, order_idx DESC),
-        o AS (
-        INSERT INTO findex_db (a, w)
-        SELECT a, w FROM temp_table AS t(a,w)
-            WHERE (
-            $2::bytea IS NULL AND NOT EXISTS (SELECT 1 FROM guard_check)
-        ) OR (
-            $2::bytea IS NOT NULL AND EXISTS (
-                SELECT 1 FROM guard_check WHERE w = $2::bytea
-            )
-        )
-        ON CONFLICT (a) DO UPDATE SET w = EXCLUDED.w
-        )
-         SELECT COALESCE((SELECT w FROM guard_check)) AS original_guard_value;
-        ",
-                &[
-                    &*guard.0,
-                    match &guard.1 {
-                        Some(g) => g,
-                        None => &None::<&[u8]>,
-                    },
-                    &addresses,
-                    &words,
-                ],
-            )
+            .query_opt(&*self.insert_stmt, &[
+                &*guard.0,
+                match &guard.1 {
+                    Some(g) => g,
+                    None => &None::<&[u8]>,
+                },
+                &addresses,
+                &words,
+            ])
             .await?
             .map_or(Ok(None), |row| {
                 row.try_get::<_, Option<&[u8]>>(0)?
@@ -210,6 +276,8 @@ impl<const ADDRESS_LENGTH: usize, const WORD_LENGTH: usize> MemoryADT
 
 #[cfg(test)]
 mod tests {
+    use tokio_postgres::NoTls;
+
     use super::*;
     use crate::{
         ADDRESS_LENGTH, Address, WORD_LENGTH,
@@ -223,91 +291,45 @@ mod tests {
 
     #[tokio::test]
     async fn test_rw_seq() -> Result<(), PostgresMemoryError> {
-        let m =
-            PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_URI).await?;
-
+        let (client, connection) = tokio_postgres::connect(DB_URI, NoTls).await?;
+        let m = PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect::<NoTls>(
+            client, connection,
+        )
+        .await?;
         test_single_write_and_read::<WORD_LENGTH, _>(&m, rand::random()).await;
-
         Ok(())
     }
 
     #[tokio::test]
     async fn test_guard_seq() -> Result<(), PostgresMemoryError> {
-        let m =
-            PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_URI).await?;
+        let (client, connection) = tokio_postgres::connect(DB_URI, NoTls).await?;
+        let m = PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect::<NoTls>(
+            client, connection,
+        )
+        .await?;
         test_wrong_guard::<WORD_LENGTH, _>(&m, rand::random()).await;
-
         Ok(())
     }
 
     #[tokio::test]
     async fn test_collision_seq() -> Result<(), PostgresMemoryError> {
-        let m =
-            PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_URI).await?;
-
+        let (client, connection) = tokio_postgres::connect(DB_URI, NoTls).await?;
+        let m = PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect::<NoTls>(
+            client, connection,
+        )
+        .await?;
         test_collisions::<WORD_LENGTH, _>(&m, rand::random()).await;
         Ok(())
     }
 
     #[tokio::test]
     async fn test_rw_ccr() -> Result<(), PostgresMemoryError> {
-        let m =
-            PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect(DB_URI).await?;
-
-        test_guarded_write_concurrent(&m, rand::random(), Some(100)).await;
+        let (client, connection) = tokio_postgres::connect(DB_URI, NoTls).await?;
+        let m = PostGresMemory::<Address<ADDRESS_LENGTH>, [u8; WORD_LENGTH]>::connect::<NoTls>(
+            client, connection,
+        )
+        .await?;
+        test_guarded_write_concurrent(&m, rand::random(), Some(1000)).await;
         Ok(())
     }
 }
-
-// Version 1 :
-// we will perform benches to know which implementation is faster, but in theory the
-// second one must be faster
-
-// let (ag, wg) = (guard.0, guard.1.as_ref().map(|w| &w[..]));
-
-// let params = match &wg {
-//     Some(guard) => vec![&*ag as &(dyn ToSql + Sync), guard],
-//     None => vec![&*ag, &None::<&[u8]> as &(dyn ToSql + Sync)],
-// }
-// .iter()
-// .copied()
-// .chain(bindings.iter().flat_map(|(a, w)| vec![&**a as _, &*w as _]))
-// .collect::<Vec<&(dyn ToSql + Sync)>>();
-
-// let query = &format!(
-//     "
-//     WITH
-//     guard_check AS (
-//         SELECT w FROM findex_db WHERE a = $1::bytea
-//     ),
-//     insert_condition AS (
-//         INSERT INTO findex_db (a, w)
-//         SELECT * FROM (
-//             VALUES
-//             {}
-//         ) AS new_values(a, w)
-// WHERE (
-//     $2::bytea IS NULL AND NOT EXISTS (SELECT 1 FROM guard_check)
-// ) OR (
-//     $2::bytea IS NOT NULL AND EXISTS (
-//        SELECT 1 FROM guard_check WHERE w = $2::bytea
-//     )
-// )
-//         ON CONFLICT (a) DO UPDATE SET w = EXCLUDED.w
-//     )
-//     SELECT COALESCE((SELECT w FROM guard_check)) AS original_guard_value;
-// ",
-//     (3..=params.len())
-//         .step_by(2) // params.len() is always even
-//         .map(|i| format!("(${}::bytea, ${}::bytea)", i, i + 1))
-//         .collect::<Vec<String>>()
-//         .join(",")
-// );
-// self.client
-//     .query_opt(query, &params)
-//     .await?
-//     .map_or(Ok(None), |row| {
-//         row.try_get::<_, Option<&[u8]>>(0)?
-//             .map(|b| b.try_into())
-//             .map_or(Ok(None), |r| Ok(Some(r?)))
-//     })
