8c633b07c67095fbf2771680e6474a476d5bd7ab
Théophile BRÉZOT <theophile.brezot@cosmian.com>
2025-04-02 16:31:28 +0200

    add concurrent benches

--- src/test_utils/benches.rs
+++ src/test_utils/benches.rs
@@ -46,8 +46,8 @@ fn build_benchmarking_keywords_index(
         .collect()
 }
 
-/// Benches searching for keywords on the given memory scaling the number of
-/// associated values from 1 to 10^4 evenly divided into N+1 samples.
+/// Benches the time required to search for a given keyword, varying the number
+/// of values indexed under this keyword.
 pub fn bench_memory_search_multiple_bindings<
     Memory: Clone + Send + Sync + MemoryADT<Address = Address<ADDRESS_LENGTH>, Word = [u8; WORD_LENGTH]>,
 >(
@@ -83,6 +83,8 @@ pub fn bench_memory_search_multiple_bindings<
     }
 }
 
+/// Bench the time required to a group of parallel processes to search for a
+/// keyword, varying the number of processes.
 pub fn bench_memory_search_multiple_keywords<
     Memory: 'static
         + Clone
@@ -148,6 +150,8 @@ pub fn bench_memory_search_multiple_keywords<
     }
 }
 
+/// Bench the time required to insert new values under a given label, varying
+/// the number of inserted values.
 pub fn bench_memory_insert_multiple_bindings<
     E: Debug,
     Memory: 'static
@@ -183,7 +187,7 @@ pub fn bench_memory_insert_multiple_bindings<
         .into_iter()
         .for_each(|(kw, vs)| rt.block_on(findex.insert(kw, vs)).unwrap());
 
-    let mut group = c.benchmark_group(format!("Multiple bindings insert ({memory_name})"));
+    let mut group = c.benchmark_group(format!("multiple-binding insert ({memory_name})"));
     for (kw, vs) in index.into_iter() {
         group.bench_function(BenchmarkId::from_parameter(vs.len()), |b| {
             b.iter_batched(
@@ -198,6 +202,9 @@ pub fn bench_memory_insert_multiple_bindings<
     }
 }
 
+/// Bench the time required for a group of process to perform insertions, both
+/// in parallel (targeting different keywords) and in concurrence (targeting the
+/// same keyword), varying the number of processes.
 pub fn bench_memory_contention<
     E: Debug,
     Memory: 'static
@@ -283,14 +290,148 @@ pub fn bench_memory_contention<
                 b.iter_batched(
                     || {
                         rt.block_on(clear(&m)).unwrap();
-                        (0..n_clients).map(|_| findex.clone()).zip(bindings.clone())
+                        (
+                            Vec::with_capacity(n_clients),
+                            (0..n_clients).map(|_| findex.clone()).zip(bindings.clone()),
+                        )
                     },
-                    |iterator| {
+                    |(mut handles, iterator)| {
                         rt.block_on(async {
-                            join_all(iterator.map(|(findex, (kw, vs))| {
-                                tokio::spawn(async move { findex.insert(kw, vs).await })
-                            }))
+                            for (findex, (kw, vs)) in iterator {
+                                let h = tokio::spawn(async move { findex.insert(kw, vs).await });
+                                handles.push(h);
+                            }
+                            join_all(handles).await
+                        })
+                    },
+                    criterion::BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+}
+
+/// Bench the time required to process both a read and an insertion, varying the
+/// number of concurrent processes performing insertions (on the same keyword).
+pub fn bench_memory_one_to_many<
+    E: Debug,
+    Memory: 'static
+        + Clone
+        + Send
+        + Sync
+        + MemoryADT<Address = Address<ADDRESS_LENGTH>, Word = [u8; WORD_LENGTH]>,
+>(
+    memory_name: &str,
+    n: usize,
+    m: impl AsyncFn() -> Memory,
+    c: &mut Criterion,
+    clear: impl AsyncFn(&Memory) -> Result<(), E>,
+    rng: &mut impl CryptoRngCore,
+) {
+    // Redis memory backend requires a tokio runtime, and all operations to
+    // happen in the same runtime, otherwise the connection returns a broken
+    // pipe error.
+    let rt = Builder::new_multi_thread()
+        .worker_threads(8)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let m = rt.block_on(m());
+
+    let findex = Arc::new(Findex::new(
+        MemoryEncryptionLayer::new(&Secret::random(rng), m.clone()),
+        dummy_encode::<WORD_LENGTH, _>,
+        dummy_decode,
+    ));
+
+    {
+        let mut group = c.benchmark_group(format!("one search to many insertions ({memory_name}"));
+        for x in make_scale(1, MAX_VAL, n) {
+            let n_clients = x.ceil() as usize;
+
+            // All clients use the same keyword and values.
+            let kw = rng.next_u64().to_be_bytes();
+            let vs = HashSet::<[u8; 8]>::from_iter([rng.next_u64().to_be_bytes()]);
+
+            group.bench_function(BenchmarkId::from_parameter(n_clients), |b| {
+                b.iter_batched(
+                    || {
+                        rt.block_on(clear(&m)).unwrap();
+                        (
+                            Vec::with_capacity(n_clients),
+                            (0..n_clients).map(|_| (findex.clone(), vs.clone())),
+                        )
+                    },
+                    |(mut handles, iterator)| {
+                        rt.block_on(async {
+                            for (findex, vs) in iterator {
+                                handles.push(tokio::spawn(async move {
+                                    loop {
+                                        findex.insert(kw, vs.clone()).await.unwrap();
+                                    }
+                                }));
+                            }
+
+                            let findex = findex.clone();
+                            tokio::spawn(async move {
+                                let kw = kw;
+                                findex.search(&kw).await.unwrap();
+                            })
                             .await
+                            .unwrap();
+
+                            for h in handles {
+                                h.abort();
+                            }
+                        })
+                    },
+                    criterion::BatchSize::SmallInput,
+                );
+            });
+        }
+    }
+
+    {
+        let mut group =
+            c.benchmark_group(format!("one insertion to many insertions ({memory_name}"));
+        for x in make_scale(1, MAX_VAL, n) {
+            let n_clients = x.ceil() as usize;
+
+            // All clients use the same keyword and values.
+            let kw = rng.next_u64().to_be_bytes();
+            let vs = HashSet::<[u8; 8]>::from_iter([rng.next_u64().to_be_bytes()]);
+
+            group.bench_function(BenchmarkId::from_parameter(n_clients), |b| {
+                b.iter_batched(
+                    || {
+                        rt.block_on(clear(&m)).unwrap();
+                    },
+                    |_| {
+                        rt.block_on(async {
+                            let mut handles = Vec::new();
+                            for _ in 0..n_clients {
+                                let findex = findex.clone();
+                                let vs = vs.clone();
+                                handles.push(tokio::spawn(async move {
+                                    loop {
+                                        findex.insert(kw, vs.clone()).await.unwrap();
+                                    }
+                                }));
+                            }
+
+                            let findex = findex.clone();
+                            let vs = vs.clone();
+                            tokio::spawn(async move {
+                                let kw = kw;
+                                findex.insert(&kw, vs).await.unwrap();
+                            })
+                            .await
+                            .unwrap();
+
+                            for h in handles {
+                                h.abort();
+                            }
                         })
                     },
                     criterion::BatchSize::SmallInput,
