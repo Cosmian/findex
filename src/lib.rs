bb4aac85f482005968aec1d5d90cef8c4f7e6e17
Théophile BRÉZOT <theophile.brezot@cosmian.com>
2025-04-14 12:21:21 +0200

    justify the `#![allow(...)]` macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 #![warn(clippy::all, clippy::nursery, clippy::cargo)]
+// This is necessary since CryptoCore depends on pkcs8 which depends on an old
+// version of rand_core, which depends on an old version of getrandom (0.2.15),
+// while CryptoCore also depends on gensym which depends on uuid, which depends
+// on a newer version of getrandom (0.3.2).
 #![allow(clippy::multiple_crate_versions)]
 
 mod address;
